0bdc4d4d13313f2b06fdeeeca39d6f96ad2e30f2
subudei <milan_cirkovic@yahoo.com>
2023-08-31 15:01:07 +0200

    feat(user-entity): add serializer

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude } from 'class-transformer';
 import {
   AfterInsert,
   AfterRemove,
@@ -15,6 +16,7 @@ export class User {
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
 
   @AfterInsert()
