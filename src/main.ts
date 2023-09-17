2329bdcbf252255f2570bc6ab30752fd8376f5f5
subudei <milan_cirkovic@yahoo.com>
2023-09-17 12:49:21 +0200

    build: cookie-session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// cookieSession has to be imported this way because of the typescript.config setup
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['asdf'], // for now we are using a single key, but in production we should use an array of keys
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
